fc36e347207d5fc2e67cbab8f8c0f10e5e9f4a27
le402n48\uer <lin0968329658@gmail.com>
2023-12-06 16:29:23 +0800

    擷取網頁標題

--- 1206.r
+++ 1206.r
@@ -2,7 +2,17 @@ URL = "https://reg.ntuh.gov.tw/EmgInfoBoard/NTUHEmgInfo.aspx"
 
 txt = scan(URL, what = "character", encoding = "UTF-8", quiet = TRUE)
 
-head(txt, 15) 
 #print前15行
+head(txt, 15) 
 
+#拼接一起
+txt_new = paste(txt, sep = "", collapse = " ")
 
+#找出TITLE
+TITLE.pos = gregexpr("<title>.*</title>", txt_new)
+start.TITLE.pos = TITLE.pos[[1]][1]
+end.TITLE.pos = start.TITLE.pos + attr(TITLE.pos[[1]], "match.length")[1] - 1
+
+TITLE.word = substr(txt_new, start.TITLE.pos, end.TITLE.pos)
+
+TITLE.word
