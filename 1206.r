126b98ed35d8c7e9b0e9000b2a30e85def55fa08
le402n48\uer <lin0968329658@gmail.com>
2023-12-06 17:01:55 +0800

    調閱最近10篇徵男文

--- 1206.r
+++ 1206.r
@@ -40,7 +40,7 @@ sub_txt = gsub('</?td>', '', sub_txt)
 sub_txt = gsub(' ', '', sub_txt)
 sub_txt
 
-
+#練習1
 #完整函式
 NTU_info = function () {
   
@@ -116,6 +116,59 @@ article_info = sub_website %>% html_nodes(".article-meta-value")
 article_info
 
 
+#練習2
+URL = "https://www.ptt.cc/bbs/AllTogether/index.html"
+website = read_html(URL)
 
+website %>% html_nodes("a") %>% .[8] %>% html_attr("href")
 
+#完整函式
+my_table = matrix("", nrow = 10, ncol = 4)
+colnames(my_table) = c("Title", "url", "ID", "time")
+
+URL = "https://www.ptt.cc/bbs/AllTogether/index.html"
+current_id = 1
+
+for (i in 1:10) {
+  
+  website = read_html(URL)
+  needed_html = website %>% html_nodes("a")
+  needed_txt = needed_html %>% html_text()
+  intrested_pos = grep("[徵男]", needed_txt, fixed = TRUE)
+  
+  if (length(intrested_pos) > 0) {
+    
+    for (j in intrested_pos) {
+      
+      if (current_id <= 10) {
+        my_table[current_id, 1] = needed_txt[j]
+        my_table[current_id, 2] = needed_html[j] %>% html_attr("href")
+      }
+      
+      current_id = current_id + 1
+      
+    }
+    
+  }
+  
+  if (current_id > 10) {
+    break
+  }
+  
+  next_page = website %>% html_nodes("a") %>% .[8] %>% html_attr("href")
+  URL = paste0("https://www.ptt.cc", next_page, sep = "")
+  
+}
+
+for (i in 1:nrow(my_table)) {
+  
+  sub_URL = paste("https://www.ptt.cc", my_table[i, 2], sep = "")
+  sub_website = read_html(sub_URL)
+  article_info = sub_website %>% html_nodes(".article-meta-value") %>% html_text()
+  my_table[i, 3] = article_info[1]
+  my_table[i, 4] = article_info[4]
+  
+}
+
+my_table
 
